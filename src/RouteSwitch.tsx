0d2050b5ce9eb2633ea60b693d840264a719f595
Tudor <pagu.tudor@gmail.com>
2022-08-30 13:11:54 +0300

    Add boilerplate

---
+++ src/RouteSwitch.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+
+function RouteSwitch() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default RouteSwitch;
